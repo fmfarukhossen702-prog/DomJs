38b526356c3566ec342db396b8d0fdd65c1d1d9d
fmfarukhossen702 <fmfarukhossen702@gmail.com>
2026-06-09 00:51:16 +0600

    add

--- script.js
+++ script.js
@@ -4,21 +4,54 @@ let ulList = document.getElementById("ulList")
 
 
 
-function handleClick () {
+function handleClick() {
+  let list = document.createElement("li");
+  let delet = document.createElement("button");
 
-    if (text.value){
+  // delet style
+  delet.innerHTML =
+    '<i class="fa-solid fa-trash-can" style="color: rgb(1, 1, 1);"></i>';
+  delet.style.fontSize = " 12px ";
+  list.style.display = "flex";
+  list.style.justifyContent = "space-between";
+  delet.style.color = "white";
+  delet.style.marginLeft = "10px";
 
-        let list1 = document.createElement("li")
-        list1.innerHTML = text.value
-        ulList.appendChild(list1)
+  list.style.paddingRight = "10px";
+  list.style.paddingLeft = "10px";
+  list.style.height = "35px";
+  list.style.border = "1px solid white";
+  list.style.alignItems = "center";
+  list.style.borderRadius = "10px";
+  list.style.marginBottom = "10px"
 
-        let list2 = document.createElement("li")
-        list2.innerHTML = text.value
-        ulList.appendChild(list2)
+  delet.onclick = function () {
+    list.remove();
+  };
+  let deletAndEite = document.createElement("div");
 
-        let list3 = document.createElement("li")
-        list3.innerHTML = text.value
-        ulList.appendChild(list3)
-        
-    }
+  let btnEite = document.createElement("button");
+  btnEite.innerHTML = "Eite";
+
+  // btnEite style
+  btnEite.style.fontSize = "12px";
+  btnEite.style.border = "1px solid white";
+  btnEite.style.paddingTop = "3px";
+  btnEite.style.paddingBottom = "3px";
+  btnEite.style.paddingRight = "8px";
+  btnEite.style.paddingLeft = "8px";
+  btnEite.style.borderRadius = "5px";
+  btnEite.style.backgroundColor = "black";
+
+//   btnEite click abble 
+  btnEite.onclick = function () {
+   
+  }
+
+
+  list.innerHTML = text.value;
+  ulList.appendChild(list);
+  list.appendChild(deletAndEite);
+  deletAndEite.appendChild(btnEite);
+  deletAndEite.appendChild(delet);
 }
